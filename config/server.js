b486fc548318d3ccfd3d71d2d5ba0241b5e4611d
Arthur Andrade <arthurandrade@MacBook-Pro-de-Arthur.local>
2021-09-05 19:50:45 -0300

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '04df7f0d53323303b4b460fa050301e4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '73011ffc-fe8b-47b8-be00-e8b75f60fbe1'
+};
